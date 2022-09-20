8763c67a2b8221f39918c730d41bfdc0807ad11a
gabrielkristesashvili <qristesashvili0@gmail.com>
2022-09-20 12:57:27 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+//      ----------------  Page Cursor  -------------------
+document.getElementsByTagName("body")[0].addEventListener("mousemove", function (n) {
+        t.style.left = n.clientX + "px",
+                t.style.top = n.clientY + "px",
+                e.style.left = n.clientX + "px",
+                e.style.top = n.clientY + "px"
+});
+var t = document.getElementById("cursor"),
+        e = document.getElementById("cursor2"),
+        i = document.getElementById("cursor3");
+function n(t) {
+        e.classList.add("hover"), i.classList.add("hover")
+}
+function s(t) {
+        e.classList.remove("hover"), i.classList.remove("hover")
+}
+s();
+for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+        o(r[a])
+}
+function o(t) {
+        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+}
+//      ------------------------------------------------
+
+
+$(document).ready(function () {
+        $('.toggle-btn').click(function () {
+                $(this).toggleClass('active');
+                $('.navbar-nav').toggleClass('active');
+        });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
